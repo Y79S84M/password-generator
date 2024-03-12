0b9c5b68eed174756e153e3e692cd5adbac76cf6
Y79S84M <yostm.dev@gmail.com>
2024-03-13 00:03:35 +0100

    init. project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Password generator
+
+Basic and simple password generator in java.
+
+## Basic recipe for a strong password
+
+- the generated password must be at least 16 characters
+- the generated password must be at max 25 characters
+- salt & pepper
---
+++ src/main/java/fr/ysaintmartin/pwgen/PasswordGenerator.java
@@ -0,0 +1,15 @@
+package fr.ysaintmartin.pwgen;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Password generator is running ...");
+		
+		// Generate password
+				
+		System.exit(0);
+		
+	}
+
+}
